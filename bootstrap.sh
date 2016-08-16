1cc3ede25264d19de6b04388a90cf810331ed836
Ubuntu <taisyunn@harusugite12.dna3vzoa4mqereoed5ayrizhpf.lx.internal.cloudapp.net>
2016-08-16 03:59:06 +0000

    Changed spark default

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-if [ ! -d "/hadoop/hdfs/cache/dfs/name/current" ]; then
-    #initialize name node
-    sudo -u hdfs hdfs namenode -format
-
-fi
-
-# setting spark defaults
-echo spark.yarn.jar hdfs:///spark/spark-assembly-1.6.2-hadoop2.6.0.jar > $SPARK_HOME/conf/spark-defaults.conf
-cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.properties
-
 
 # When using service command it says "Failed to get D-Bus connection: Operation not permitted"
 # There is 3 ways to work around this phenomenon.
@@ -18,15 +8,14 @@ cp $SPARK_HOME/conf/metrics.properties.template $SPARK_HOME/conf/metrics.propert
 # 3. Avoid using service command and just use the daemon script directly
 # And my choice is 3. because the feature provided in service command is not actually needed at this morment.
 
-#service hadoop-hdfs-namenode start
-/etc/rc3.d/S85hadoop-hdfs-namenode start
+/etc/rc3.d/S85hadoop-hdfs-datanode start
 
 if [ $? -gt 0 ]; then
     echo "Starting daemon failed."
     exit 1
 fi
 
-/etc/rc3.d/S85hadoop-yarn-resourcemanager start
+/etc/rc3.d/S85hadoop-yarn-nodemanager start
 
 if [ $? -gt 0 ]; then
     echo "Starting daemon failed."
